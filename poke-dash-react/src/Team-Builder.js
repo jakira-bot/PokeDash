67edd5cefcc28a246e2354a62a871f2daa8fd34f
Darian Hutchinson <dhutch2@pdx.edu>
2021-05-12 18:21:53 -0700

    Started working on the TeamBuilder Page

--- poke-dash-react/src/Team-Builder.js
+++ poke-dash-react/src/Team-Builder.js
@@ -1,7 +1,18 @@
+import './Team-Builder.css';
 export default function TeamBuilder() {
     return (
       <>
         <h1>Team-Builder</h1>
+        <div class="teamContainer">
+          <div class="teamElement col">+</div>
+          <div class="teamElement col">+</div>
+          <div class="teamElement col">+</div>
+        </div>
+        <div class="teamContainer">
+          <div class="teamElement col lowerTeam">+</div>
+          <div class="teamElement col lowerTeam">+</div>
+          <div class="teamElement col lowerTeam">+</div>
+        </div>
       </>
     );
   }
