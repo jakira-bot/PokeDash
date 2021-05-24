f70c09e4c5bcd6a5e9fb1736b3b592adad574a28
canthony-pdx <canthony@pdx.edu>
2021-05-24 01:09:47 -0700

    currently logs array of locations in a region

--- poke-dash-react/src/Map.js
+++ poke-dash-react/src/Map.js
@@ -1,7 +1,50 @@
-export default function Map() {
-    return (
-      <>
-        <h1>Map</h1>
-      </>
-    );
+import { useState, useEffect } from "react";
+
+export default function Search() {
+  const [map, setMap] = useState(null);
+  const [search, setSearch] = useState("0");
+
+  useEffect(() => {
+    getMap();
+  }, [search]);
+
+  async function getMap() {
+    try {
+      let response = await fetch(`https://pokeapi.co/api/v2/region/${search}`);
+      let data = await response.json();
+      setMap(data);
+      console.log(data);
+      let locations = data.locations;
+      console.log(locations);
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setSearch(event.target.value)
+  }
+
+  return (
+    <>
+      <form className="mt-2 ml-2">
+        <h2>Search</h2>
+        <label htmlFor="item"> Search for a map region (e.g kanto)</label>
+        <input type="text" onChange={handleChange} />
+        <input className="btn btn-primary" type="submit" value="Search" />
+          {/* <select class="select_options" id="search">
+            <option value="">Choose an Option</option>
+            <option value="kanto">Kanto</option>
+            <option value="johto">Johto</option>
+          </select> */}
+      </form>
+
+      {map && (
+        <div>
+
+        </div>
+      )}
+    </>
+  );
+}
