4d846c6fb8dd41cc97fd19d4e25fa34adfea0fd3
canthony-pdx <canthony@pdx.edu>
2021-05-25 12:00:10 -0700

    type in region, shows locations with url (up to gen 7)

--- poke-dash-react/src/Map.js
+++ poke-dash-react/src/Map.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 
 export default function Search() {
-  const [map, setMap] = useState(null);
-  const [search, setSearch] = useState("0");
+  const [map, setMap] = useState("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getMap();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search]);
 
   async function getMap() {
@@ -13,18 +14,18 @@ export default function Search() {
       let response = await fetch(`https://pokeapi.co/api/v2/region/${search}`);
       let data = await response.json();
       setMap(data);
-      console.log(data);
-      let locations = data.locations;
-      console.log(locations);
+      //console.log(data);
     } catch (error) {
       console.log(error);
     }
   }
-
   function handleChange(event) {
     console.log(event.target.value);
     setSearch(event.target.value)
   }
+
+  let locations = map.locations;
+  console.log(locations);
 
   return (
     <>
@@ -39,10 +40,17 @@ export default function Search() {
             <option value="johto">Johto</option>
           </select> */}
       </form>
-
-      {map && (
+      {locations && (
         <div>
-
+          {
+            locations.map((anObjectMapped, index) => {
+              return (
+                  <p key={`${anObjectMapped.name}_{anObjectMapped.url}`}>
+                      {anObjectMapped.name} - {anObjectMapped.url}
+                  </p>
+              );
+          })
+          }
         </div>
       )}
     </>
