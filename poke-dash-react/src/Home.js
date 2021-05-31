ac49bf5959040ffc05cc6e1df391fe37f87c0ac8
Jerry Smedley <smedley@ada.cs.pdx.edu>
2021-05-30 19:31:08 -0700

    Added Set up individual get from the pokeapi for 1 pokemon

--- poke-dash-react/src/Home.js
+++ poke-dash-react/src/Home.js
@@ -1,10 +1,18 @@
 export default function Home() {
 
   let url = 'https://pokeapi.co/api/v2/pokemon?limit=898",';
+  let search = 'https://pokeapi.co/api/v2/pokemon/';
 
   let count = 0;
   
   let list = [];
+
+  async function pokeGrab(pname) {
+    let partA = await fetch([search,pname].join(''));
+    let partB = await partA.json();
+    console.log(partB);
+    return await partB;
+  }
   
   let  xhr = new XMLHttpRequest();
   xhr.open ('GET', url, true);
@@ -13,10 +21,13 @@ export default function Home() {
   
   xhr.onload = () => {
       if ( xhr.status === 200) {
-          console.log(xhr.response);
+          //console.log(xhr.response);
           for (const mon of xhr.response.results){
           }
           list = xhr.response.results;
+          console.log(list[0].name);
+          let current = pokeGrab(list[0].name);
+          current = current.then( (data) => console.log(data.abilities));
       }
       else {
           console.log( `Status Code: ${xhr.status} - ${xhr.statusText }`) ;
