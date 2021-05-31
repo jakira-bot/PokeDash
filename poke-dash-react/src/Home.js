23fd71e888f6ca4c3df104c72e30bc3c32ba2b00
Jerry Smedley <smedley@ada.cs.pdx.edu>
2021-05-30 20:24:33 -0700

    Added Image to home page

--- poke-dash-react/src/Home.js
+++ poke-dash-react/src/Home.js
@@ -7,6 +7,11 @@ export default function Home() {
   
   let list = [];
 
+  let pokeI = ["bulbasaur",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+  
+  ]
+
   async function pokeGrab(pname) {
     let partA = await fetch([search,pname].join(''));
     let partB = await partA.json();
@@ -14,29 +19,47 @@ export default function Home() {
     return await partB;
   }
   
-  let  xhr = new XMLHttpRequest();
-  xhr.open ('GET', url, true);
-  xhr.responseType = 'json';
-  xhr.send();
+
+  function pokeStart(num) {
+    let  xhr = new XMLHttpRequest();
+    xhr.open ('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.send();
+    
+    xhr.onload = () => {
+        if ( xhr.status === 200) {
+            //console.log(xhr.response);
+            for (const mon of xhr.response.results){
+            }
+            list = xhr.response.results;
+            console.log(list[num].name);
+            let current = pokeGrab(list[num].name);
+            current = current.then( (data) => {
+              console.log(data.abilities);
+              //let pic = document.createElement('img');
+              //pic.src = data.sprites.other.dream_world.front_default;
+              //pic.style.display = "block";
+              return [data.name, data.sprites.other.dream_world.front_default]
+            });
+        }
+        else {
+            console.log( `Status Code: ${xhr.status} - ${xhr.statusText }`) ;
+        }
+    };
+  }
   
-  xhr.onload = () => {
-      if ( xhr.status === 200) {
-          //console.log(xhr.response);
-          for (const mon of xhr.response.results){
-          }
-          list = xhr.response.results;
-          console.log(list[0].name);
-          let current = pokeGrab(list[0].name);
-          current = current.then( (data) => console.log(data.abilities));
-      }
-      else {
-          console.log( `Status Code: ${xhr.status} - ${xhr.statusText }`) ;
-      }
-  };
+  pokeStart(count)
 
   return (
     <>
       <h1>Home</h1>
+      <div>
+        <div>
+          <img src={pokeI[1]} />
+        </div>
+
+        
+      </div>
     </>
   );
 }
