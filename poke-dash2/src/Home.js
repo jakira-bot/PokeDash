0fa23d6ca66cec48f489ba9860a20e77d7ca5f06
Akira <smedley@pdx.edu>
2024-03-03 11:13:21 -0800

    Removed all commented out code

--- poke-dash2/src/Home.js
+++ poke-dash2/src/Home.js
@@ -397,7 +397,6 @@ function keyStroke(stroke){
 async function pokeGrab(pname) {
   let partA = await fetch([search,pname].join(''));
   let partB = await partA.json();
-  //console.log(partB);
   return await partB;
 }
 
@@ -410,16 +409,11 @@ function pokeStart(num) {
   
   xhr.onload = () => {
       if ( xhr.status === 200) {
-          //console.log(xhr.response);
           for (const mon of xhr.response.results){
           }
           list = xhr.response.results;
-          //console.log(list);
           let current = pokeGrab(list[num].name);
           current = current.then( (data) => {
-            //let pic = document.createElement('img');
-            //pic.src = data.sprites.other.dream_world.front_default;
-            //pic.style.display = "block";
             return [data.name, data.sprites.other.dream_world.front_default]
           });
       }
